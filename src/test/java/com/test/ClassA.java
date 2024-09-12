50e98365a75fc8c8219ea60fa4e5a36622ebb2ab
prasanna2503 <kprasanna2406@gmail.com>
2024-09-12 19:07:46 +0530

    new project

---
+++ src/test/java/com/test/ClassA.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class ClassA {
+	public void student() {
+		System.out.println("Student name:Prasanna");
+	}
+	public void branch() {
+		System.out.println("Student name:Prasanna");
+	}
+	public void college() {
+		System.out.println("Student name:Prasanna");
+	}
+}
